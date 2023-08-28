fb5e96216c109193c7aff2dafccacfe89a6c7760
unknown <rohitthakur1122rt34@gmail.com>
2023-08-28 18:34:24 +0530

    I think its working

--- main/test.py
+++ main/test.py
@@ -4,10 +4,10 @@ import cv2
 import numpy as np
 import torch
 import RRDBNet_arch as arch
+from PIL import Image
 
-model_path = 'models/RRDB_ESRGAN_x4.pth'  # models/RRDB_ESRGAN_x4.pth OR models/RRDB_PSNR_x4.pth
-device = torch.device('cuda')  # if you want to run on CPU, change 'cuda' -> cpu
-# device = torch.device('cpu')
+model_path = 'models/RRDB_ESRGAN_x4.pth'  
+device = torch.device('cuda')  
 
 test_img_folder = 'LR/*'
 
@@ -23,7 +23,6 @@ for path in glob.glob(test_img_folder):
     idx += 1
     base = osp.splitext(osp.basename(path))[0]
     print(idx, base)
-    # read images
     img = cv2.imread(path, cv2.IMREAD_COLOR)
     img = img * 1.0 / 255
     img = torch.from_numpy(np.transpose(img[:, :, [2, 1, 0]], (2, 0, 1))).float()
@@ -35,3 +34,11 @@ for path in glob.glob(test_img_folder):
     output = np.transpose(output[[2, 1, 0], :, :], (1, 2, 0))
     output = (output * 255.0).round()
     cv2.imwrite('results/{:s}_rlt.png'.format(base), output)
+    image = Image.open('results/{:s}_rlt.png'.format(base))
+    width = 800
+    height = 800
+    new_size = (width, height)
+    resized_image = image.resize(new_size, Image.LANCZOS)
+
+    # Save as PNG to maintain quality
+    resized_image.save('results/{:s}_rlt.png'.format(base))
