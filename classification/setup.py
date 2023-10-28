8b90135c1fda3886fea0cdb7d760e3557891b626
LOOSINbl1tZ <rohitthakur1122rt34@gmail.com>
2023-10-28 16:53:22 +0530

    setup

--- classification/setup.py
+++ classification/setup.py
@@ -1,8 +1,18 @@
 import os
-import shutil
+import zipfile
 
 os.mkdir('data')
 os.mkdir('logs')
 os.mkdir('logs/fit')
 os.mkdir('model')
-os.mkdir('drive')
+os.mkdir('data_temp')
+
+data = os.path.join('drive')
+
+for i in os.listdir(data):
+    file = os.path.join(data,i)
+    folder_name = i.split('.')[0]
+    path = os.path.join('data_temp',folder_name)
+
+    with zipfile.ZipFile(file, 'r') as zip_ref:
+        zip_ref.extractall(path)
