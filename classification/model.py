428b6fe6795f4718b390c1288e8a06ac85ce6cf2
LOOSINbl1tZ <rohitthakur1122rt34@gmail.com>
2023-10-31 21:53:54 +0530

    added two models

--- classification/model.py
+++ classification/model.py
@@ -1,5 +1,7 @@
 from keras.models import Model
 from keras.layers import Input, Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from keras.layers import BatchNormalization, Activation, GlobalAveragePooling2D, Dense, Add
+from keras.models import Model, Sequential
 
 # Define the VGG19 architecture
 def VGG19(input_shape, num_classes):
@@ -56,4 +58,97 @@ def VGG19(input_shape, num_classes):
     # Create the model
     model = Model(inputs=input_tensor, outputs=x, name='vgg19')
 
+    return model
+
+def residual_unit(x, filters, stride=1):
+    identity = x
+    
+    x = Conv2D(filters, (1, 1), strides=(stride, stride), padding='valid')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    
+    x = Conv2D(filters, (3, 3), padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    
+    x = Conv2D(filters * 4, (1, 1))(x)
+    x = BatchNormalization()(x)
+    
+    if stride != 1 or identity.shape[-1] != filters * 4:
+        identity = Conv2D(filters * 4, (1, 1), strides=(stride, stride))(identity)
+    
+    x = Add()([x, identity])
+    x = Activation('relu')(x)
+    
+    return x
+
+def ResNet50(input_shape=(224, 224, 3),classes = 3):
+    input_tensor = Input(shape=input_shape)
+    
+    x = Conv2D(64, (7, 7), strides=(2, 2), padding='same')(input_tensor)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
+    
+    x = residual_unit(x, 64)
+    x = residual_unit(x, 64)
+    x = residual_unit(x, 64)
+    
+    x = residual_unit(x, 128, stride=2)
+    x = residual_unit(x, 128)
+    x = residual_unit(x, 128)
+    x = residual_unit(x, 128)
+    
+    x = residual_unit(x, 256, stride=2)
+    x = residual_unit(x, 256)
+    x = residual_unit(x, 256)
+    x = residual_unit(x, 256)
+    x = residual_unit(x, 256)
+    
+    x = residual_unit(x, 512, stride=2)
+    x = residual_unit(x, 512)
+    x = residual_unit(x, 512)
+    
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(classes, activation='softmax')(x)
+    
+    model = Model(inputs=input_tensor, outputs=x)
+    
+    return model
+
+def VGG16(input_shape=(224, 224, 3), num_classes=3):
+    model = Sequential()
+    
+    # Block 1
+    model.add(Conv2D(64, (3, 3), activation='relu', padding='same', input_shape=input_shape, name='block1_conv1'))
+    model.add(Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv2'))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool'))
+    
+    # Block 2
+    model.add(Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1'))
+    model.add(Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv2'))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool'))
+    
+    # Block 3
+    model.add(Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1'))
+    model.add(Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv2'))
+    model.add(Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv3'))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool'))
+    
+    model.add(Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1'))
+    model.add(Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv2'))
+    model.add(Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv3'))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool'))
+    
+    model.add(Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1'))
+    model.add(Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2'))
+    model.add(Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv3'))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool'))
+    
+    model.add(Flatten(name='flatten'))
+    model.add(Dense(128, activation='relu', name='fc1'))
+    model.add(Dense(256, activation='relu', name='fc2'))
+    model.add(Dense(512, activation='relu', name='fc3'))
+    model.add(Dense(num_classes, activation='softmax', name='predictions'))
+    
     return model
---
+++ classification/vgg19.py
@@ -0,0 +1,29 @@
+from model import VGG19, ResNet50, VGG16
+from data_gen import TrainGen,TestGen
+import os
+from keras.callbacks import TensorBoard
+import datetime
+
+n = 1
+
+data = TrainGen()
+train_data, val_data = data.load()
+
+input_shape = (224,224,3)
+
+vgg19_model = VGG19(input_shape,3)
+vgg19_model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+log_dir = os.path.join('logs','fit')  
+tensorboard_callback = TensorBoard(log_dir=log_dir, histogram_freq=1, write_graph=True)
+
+vgg19_model.fit(train_data,epochs=n,validation_data=val_data,callbacks=[tensorboard_callback])
+test_loss, test_accuracy = vgg19_model.evaluate(TestGen().load())
+print(test_loss,test_accuracy)
+
+time = datetime.datetime.now()
+extracted_time = [time.year,time.month,time.hour,time.minute,time.second]
+extracted_time = [str(i) for i in extracted_time]
+
+model_name = 'vgg19_'+'-'.join(extracted_time)+'.h5'
+vgg19_model.save(os.path.join('model',model_name))
