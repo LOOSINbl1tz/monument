b1f09617239cf9ad8b4f37ab63fc7c5b46783b9e
LOOSINbl1tZ <rohitthakur1122rt34@gmail.com>
2023-11-02 22:58:37 +0530

    i thing we are done

--- classification/test.py
+++ classification/test.py
@@ -1,25 +1,51 @@
 from keras.models import load_model
 import os
-from tensorflow.keras.preprocessing import image
+import matplotlib.image as mpimg
 import numpy as np
-from keras.applications.vgg19 import preprocess_input, decode_predictions
+from matplotlib import pyplot as plt
+from data_gen import TestGen
 
 path = os.path.join('model','best','resnet50_2023-10-23-39-27.h5')
 
 model = load_model(path)
-img_path = os.path.join('data','test','lotusTemple','1.png')
-img = image.load_img(img_path, target_size=(224, 224))
-x = image.img_to_array(img)
-x = np.expand_dims(x, axis=0)
-x = preprocess_input(x)
 
-predictions = model.predict(x)
+test_loss, test_accuracy = model.evaluate(TestGen().load())
+print(test_loss,test_accuracy)
+preds = model.predict(TestGen().load())
+mapper = {0:['GatewayOfIndia','Gateway Of India'],1:['CharMinar','Char Minar'],2:['lotusTemple','Lotus Temple']}
 
-mapper = {0:'Gateway Of India',1:'Char Minar',2:'Lotus Temple'}
+path_dir = os.path.join('data','test')
+path_dir_res = os.path.join('data','test_res')
 
-for val,i in enumerate(predictions[0]):
-    if i == 1.0:
-        print(mapper[val])
+for path_idx,i in enumerate(preds):
+    val = np.argmax(i)
+    monument_name = mapper[val][1]
+    monument_dir = mapper[val][0]
 
+    img_path1 = os.path.join(path_dir,monument_dir,os.listdir(os.path.join(path_dir,monument_dir))[0])
+    img_path2 = os.path.join(path_dir_res,monument_dir,os.listdir(os.path.join(path_dir_res,monument_dir))[0])
 
+    img1 = mpimg.imread(img_path1)  
+    plt.subplot(1, 3, 1)  
+    plt.imshow(img1) 
+    res_h = str(img1.shape[0])
+    res_w = str(img1.shape[1])
+    plt.title('Low Res'+" "+res_w+'x'+res_h)
+    plt.axis('off')
 
+    img2 = mpimg.imread(img_path2) 
+    plt.subplot(1, 3, 2) 
+    plt.imshow(img2) 
+    res_h = str(img2.shape[0])
+    res_w = str(img2.shape[1])
+    plt.title('High Res'+" "+res_w+'x'+res_h)
+    plt.axis('off')
+
+    plt.subplot(1, 3, 3)
+    plt.text(0.5, 0.5, 'Predicted: '+monument_name, fontsize=12, ha='center')
+    plt.axis('off')
+
+    plt.tight_layout()
+    plt.savefig(os.path.join('preds',monument_name+'.png'))
+    plt.show()
+    
